fdd3b0f2492bcca4289a4a51d617ce833c48351a
JeremyIR <jeremy.i.rosenfeld@gmail.com>
2016-03-07 19:03:22 -0800

    Refactor dance and blinkyDance from functional to pseudoclassical

--- src/blinkyDancer.js
+++ src/blinkyDancer.js
@@ -3,7 +3,7 @@ var makeBlinkyDancer = function(top, left, timeBetweenSteps) {
 
   // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
   // so we must keep a copy of the old version of this function
-  this.step();
+
 };
 
 makeBlinkyDancer.prototype = Object.create(makeDancer.prototype);
@@ -14,6 +14,8 @@ makeBlinkyDancer.prototype.step = function() {
   // toggle() is a jQuery method to show/hide the <span> tag.
   // See http://api.jquery.com/category/effects/ for this and
   // other effects you can use on a jQuery-wrapped html tag.
+  makeDancer.prototype.step.call(this);
+
   console.log('New Step');
   this.$node.toggle();
 };
